e532c54c2b5015551c9cb17500605404e9e27ee0
Sumit31-cyber <sumitkum9305@gmail.com>
2022-04-16 21:58:04 +0530

    code cleaning and Profile added

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,13 +1,56 @@
-import { Image, Pressable, ScrollView, StyleSheet } from "react-native";
+import {
+  ActivityIndicator,
+  Alert,
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 import MasonaryList from "../components/MasonaryList";
 import { Text, View } from "../components/Themed";
 import pins from "../assets/data/pins";
 
 import { Feather, MaterialIcons } from "@expo/vector-icons";
-import { useNhostClient, useSignOut } from "@nhost/react";
+import { useNhostClient, useSignOut, useUserId } from "@nhost/react";
+import { useEffect, useState } from "react";
 
 export default function ProfileScreen() {
+  const [user, setUser] = useState();
   const { signOut } = useSignOut();
+  const nhost = useNhostClient();
+  const userId = useUserId();
+
+  const GET_USER_QUERY = `
+  query MyQuery($id : uuid!) {
+  user(id: $id){
+    id,
+    avatarUrl
+    displayName
+    pins {
+      id
+      image
+      title
+      created_at
+    }
+  }
+}`;
+
+  const fetchUserData = async () => {
+    const result = await nhost.graphql.request(GET_USER_QUERY, { id: userId });
+    if (result.error) {
+      Alert.alert("Error Fetching User", result.error.message);
+    } else {
+      setUser(result.data.user);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  if (!user) {
+    return <ActivityIndicator />;
+  }
 
   return (
     <ScrollView style={styles.container}>
@@ -26,14 +69,14 @@ export default function ProfileScreen() {
         <Image
           style={styles.image}
           source={{
-            uri: "https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+            uri: "https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
           }}
         />
-        <Text style={styles.title}>Sumit @Bravo</Text>
+        <Text style={styles.title}>{user.displayName}</Text>
         <Text style={styles.subTitle}>10k Followers | 125 Followings</Text>
       </View>
 
-      <MasonaryList pins={pins} />
+      <MasonaryList pins={user.pins} onRefresh={fetchUserData} />
     </ScrollView>
   );
 }
